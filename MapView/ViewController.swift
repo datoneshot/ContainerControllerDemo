eb093493d74d301186bbde54fc87de072a68280b
datdn <datdn@ominext.com>
2019-04-23 17:39:51 +0700

    Initial Commit

---
+++ MapView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapView
+//
+//  Created by Doan Dat on 4/23/19.
+//  Copyright © 2019 Doan Dat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
